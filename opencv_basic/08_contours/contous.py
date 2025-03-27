4dfb64145640ffe77b3f94ca181d1651947c8c6f
hwan <head1ton@gmail.com>
2025-03-28 05:28:58 +0900

    contous

--- opencv_basic/08_contours/contous.py
+++ opencv_basic/08_contours/contous.py
@@ -10,7 +10,7 @@ ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
-    print(cnt)
+    print(cnt.shape)
     # print(cv2.contourArea(cnt))
     if cv2.contourArea(cnt) > 200:
         cv2.drawContours(img, cnt, -1, (0, 255, 0), 1)

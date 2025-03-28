b9cf20ff1d9fbd2ddee72156e2174e070b0ea998
hwan <head1ton@gmail.com>
2025-03-28 22:39:00 +0900

    face emotion

---
+++ face_emotion_detection/prepare_data.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+
+import os
+import cv2
+import numpy as np
+from utils import get_face_landmarks
+
+data_dir = 'data'
+
+output = []
+for emotion_index, emotion in enumerate(sorted(os.listdir(data_dir))):
+    for image_path_ in os.listdir(os.path.join(data_dir, emotion)):
+        image_path = os.path.join(data_dir, emotion, image_path_)
+
+        image = cv2.imread(image_path)
+
+        face_landmarks = get_face_landmarks(image)
+
+        face_landmarks.append(int(emotion_index))
+        output.append(face_landmarks)
+
+np.savetxt('data.txt', np.asarray(output))
